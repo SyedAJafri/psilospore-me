12f03aec75668d83172557fa6a3f0e1862e9e3d2
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-03-11 17:20:26 -0500

    moved additonalDescription, created footer,

--- src/App.js
+++ src/App.js
@@ -4,28 +4,40 @@ import '../semantic/dist/semantic.min.css';
 
 import './App.css';
 
-import { Container, GridColumn, Grid, GridRow, Segment, Item, ItemHeader, Image, Header, Icon } from 'semantic-ui-react';
+import { Container, GridColumn, Grid, GridRow, Segment, Item, ItemHeader, Image, Header, Icon, Card } from 'semantic-ui-react';
 
 class SkillBox extends Component {
+  
   render() {
+
+    let additionalDescription = [];
+    if (this.props.jobs) {
+      additionalDescription.push(<Item.Meta> <b>Where</b> {this.props.jobs.toString().replace(/,/g, ', ') } </Item.Meta>);
+    }
+    if (this.props.description) {
+      additionalDescription.push(<Item.Description> {this.props.description} </Item.Description>);
+    }
+
+    // TODO onclick expand to include more like where and description
     return (
-      <Segment circular raised className="skillbox">
+      <Card raised centered link className="skillbox">
         <Item.Group>
           <Item>
             { this.props.logo && <Item.Image size='tiny' src={this.props.logo} ></Item.Image> }
             <Item.Content>
               <ItemHeader>{this.props.skillTitle}</ItemHeader>
-              { this.props.jobs && <Item.Meta> <b>Where</b> {this.props.jobs.toString().replace(',', ', ') } </Item.Meta> }
-              { this.props.description && <Item.Description> {this.props.description} </Item.Description> }
+              {additionalDescription}
             </Item.Content>
           </Item>
         </Item.Group>
-      </Segment>
+      </Card>
     )
   }
 }
 
 class App extends Component {
+
+
   //TODO externalize jobs
   //TODO break up components further
   render() {
@@ -45,32 +57,53 @@ class App extends Component {
               <Icon link circular name='linkedin'></Icon>
               <Icon link circular name='twitter'></Icon>
             </Header>
-            <Container fluid>
-              <Header>Languages</Header>
-              <SkillBox
-                skillTitle='Javascript'
-                logo={reactLogo}
-                jobs={['Alertus', 'ITG', 'psilospore.me']}
-              ></SkillBox>
-              <SkillBox
-                skillTitle='Java'
-                jobs={['UMD', 'Alertus', 'ITG']}
-              ></SkillBox>
-              
+            <Container fluid >
+              <Header as='h3'>Languages</Header>
+              <Card.Group>
+                <SkillBox
+                  skillTitle='Javascript'
+                  jobs={['Alertus', 'ITG', 'psilospore.me']}
+                  description='ssdgdgs adsgdgasf afsafs asf  as fasfasfafs asfasfassaas afas asfa ssfas as asfasfasasf  asfasfasafs asffassaf asffasf'
+                ></SkillBox>
+                <SkillBox
+                  skillTitle='Java'
+                  jobs={['UMD', 'Alertus', 'ITG']}
+                ></SkillBox>
+                <SkillBox
+                  skillTitle='Java'
+                  jobs={['UMD', 'Alertus', 'ITG']}
+                ></SkillBox>
+                <SkillBox
+                  skillTitle='Java'
+                  jobs={['UMD', 'Alertus', 'ITG']}
+                ></SkillBox>
+                <SkillBox
+                  skillTitle='Java'
+                  jobs={['UMD', 'Alertus', 'ITG']}
+                ></SkillBox>
+              </Card.Group>
             </Container>
-            <Container fluid>
-              <Header>Tools, Libraries, and Frameworks</Header>
-              <SkillBox
-                skillTitle='React JS'
-                logo={logo}
-                jobs={['psilospore.me']}
-              ></SkillBox>
-              <SkillBox
-                skillTitle='Spring Framework'
-                jobs={['ITG']}
-              ></SkillBox>
+            <pre></pre>
+            <Container fluid >
+              <Header as='h3'>Tools, Libraries, and Frameworks</Header>
+              <Card.Group>              
+                <SkillBox
+                  skillTitle='React JS'
+                  logo={reactLogo}
+                  jobs={['psilospore.me']}
+                ></SkillBox>
+                <SkillBox
+                  skillTitle='Spring Framework'
+                  jobs={['ITG']}
+                ></SkillBox>
+                </Card.Group>
             </Container>
           </Container>
+          <Segment inverted fluid vertical className='footer'>
+            <Header.Subheader as='h4' >
+              Built with React JS + semantic ui
+            </Header.Subheader>
+          </Segment>
       </div>
     );
   }

54b0ff3c63abbf37f961e6ecd40e2293fbd5a3c9
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-03-11 14:14:55 -0500

    containers for frameworks, tools, and one for languaguages
    added additonal skills
    added social media links
    passing jobs, description, and logo as props now
    skills boxes are now circular

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import reactLogo from './logo.svg';
 import '../semantic/dist/semantic.min.css';
 
 import './App.css';
 
-import { Container, GridColumn, Grid, GridRow, Segment, Item, ItemHeader } from 'semantic-ui-react';
+import { Container, GridColumn, Grid, GridRow, Segment, Item, ItemHeader, Image, Header, Icon } from 'semantic-ui-react';
 
 class SkillBox extends Component {
   render() {
     return (
-      <Segment raised className="skillbox">
+      <Segment circular raised className="skillbox">
         <Item.Group>
           <Item>
-            <ItemHeader as='a'>React</ItemHeader>
+            { this.props.logo && <Item.Image size='tiny' src={this.props.logo} ></Item.Image> }
+            <Item.Content>
+              <ItemHeader>{this.props.skillTitle}</ItemHeader>
+              { this.props.jobs && <Item.Meta> <b>Where</b> {this.props.jobs.toString().replace(',', ', ') } </Item.Meta> }
+              { this.props.description && <Item.Description> {this.props.description} </Item.Description> }
+            </Item.Content>
           </Item>
         </Item.Group>
       </Segment>
@@ -21,14 +26,50 @@ class SkillBox extends Component {
 }
 
 class App extends Component {
+  //TODO externalize jobs
+  //TODO break up components further
   render() {
     return (
       <div className="App">
-          <Segment attached>
-            Psilospore
-          </Segment>
           <Container fluid>
-            <SkillBox></SkillBox>
+            <Header dividing>
+              <Header.Content as='h1'>
+                <Icon name='plug' fitted></Icon>
+                Syed A Jafri
+              </Header.Content>
+              <Header.Subheader>
+                @psilospore
+              </Header.Subheader>
+              <Icon link circular name='github'></Icon>
+              <Icon link circular name='stack overflow'></Icon>
+              <Icon link circular name='linkedin'></Icon>
+              <Icon link circular name='twitter'></Icon>
+            </Header>
+            <Container fluid>
+              <Header>Languages</Header>
+              <SkillBox
+                skillTitle='Javascript'
+                logo={reactLogo}
+                jobs={['Alertus', 'ITG', 'psilospore.me']}
+              ></SkillBox>
+              <SkillBox
+                skillTitle='Java'
+                jobs={['UMD', 'Alertus', 'ITG']}
+              ></SkillBox>
+              
+            </Container>
+            <Container fluid>
+              <Header>Tools, Libraries, and Frameworks</Header>
+              <SkillBox
+                skillTitle='React JS'
+                logo={logo}
+                jobs={['psilospore.me']}
+              ></SkillBox>
+              <SkillBox
+                skillTitle='Spring Framework'
+                jobs={['ITG']}
+              ></SkillBox>
+            </Container>
           </Container>
       </div>
     );

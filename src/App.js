cc77e9356836c11fad064f52459cbf6ef2563965
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-03-12 18:56:24 -0400

    Moved social media to footer, Created pages

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import '../semantic/dist/semantic.min.css';
 
 import './App.css';
 
-import { Container, GridColumn, Grid, GridRow, Segment, Item, ItemHeader, Image, Header, Icon, Card } from 'semantic-ui-react';
+import { Container, GridColumn, Grid, GridRow, Segment, Item, ItemHeader, Image, Header, Icon, Card, Dimmer, DimmerDimmable } from 'semantic-ui-react';
 
 class SkillBox extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {active: false};
+  }
   
   render() {
 
     let additionalDescription = [];
     if (this.props.jobs) {
-      additionalDescription.push(<Item.Meta> <b>Where</b> {this.props.jobs.toString().replace(/,/g, ', ') } </Item.Meta>);
+      additionalDescription.push(<Item.Meta key={this.props.id + "_where"}> <b>Where</b> {this.props.jobs.toString().replace(/,/g, ', ') } </Item.Meta>);
     }
     if (this.props.description) {
-      additionalDescription.push(<Item.Description> {this.props.description} </Item.Description>);
+      additionalDescription.push(<Item.Description key={this.props.id + "_description"}> {this.props.description} </Item.Description>);
     }
 
     // TODO onclick expand to include more like where and description
     return (
-      <Card raised centered link className="skillbox">
+      <Card raised link className="skillbox" onClick={() => this.setState({active: !this.state.active})}>
         <Item.Group>
           <Item>
             { this.props.logo && <Item.Image size='tiny' src={this.props.logo} ></Item.Image> }
@@ -35,76 +40,154 @@ class SkillBox extends Component {
   }
 }
 
-class App extends Component {
+const Footer = (props) => {
+  return (
+      <Segment inverted vertical className='footer'>
+        <Icon link circular name='github'></Icon>
+        <Icon link circular name='stack overflow'></Icon>
+        <Icon link circular name='linkedin'></Icon>
+        <Icon link circular name='twitter'></Icon>
+        <Header.Subheader as='h4' >
+          Built with React JS + semantic ui
+        </Header.Subheader>
 
+      </Segment>
+    )
+}
 
-  //TODO externalize jobs
-  //TODO break up components further
+class IntroPage extends Component {
   render() {
     return (
-      <div className="App">
-          <Container fluid>
-            <Header>
-              <Header.Content as='h1'>
-                <Icon name='plug' fitted></Icon>
-                <a id='name-title'>Syed A Jafri</a>
-                <Header.Subheader id='alias'>
-                  @psilospore
-                </Header.Subheader>
-              </Header.Content>
+      <Header>
+        <Header.Content as='h1'>
+          <Icon name='plug' fitted></Icon>
+          <a id='name-title'>Syed A Jafri</a>
+          <Header.Subheader id='alias'>
+            @psilospore
+          </Header.Subheader>
+        </Header.Content>
+      </Header>
+    )
+  }
+}
 
-              <Icon link circular name='github'></Icon>
-              <Icon link circular name='stack overflow'></Icon>
-              <Icon link circular name='linkedin'></Icon>
-              <Icon link circular name='twitter'></Icon>
-            </Header>
-            <Container fluid >
-              <Header as='h3' dividing>Languages</Header>
-              <Card.Group>
-                <SkillBox
-                  skillTitle='Javascript'
-                  jobs={['Alertus', 'Jibe', 'ITG', 'psilospore.me']}
-                  description='ssdgdgs adsgdgasf afsafs asf  as fasfasfafs asfasfassaas afas asfa ssfas as asfasfasasf  asfasfasafs asffassaf asffasf'
-                ></SkillBox>
-                <SkillBox
-                  skillTitle='Java'
-                  jobs={['UMD', 'Alertus', 'ITG']}
-                ></SkillBox>
-                <SkillBox
-                  skillTitle='Java'
-                  jobs={['UMD', 'Alertus', 'ITG']}
-                ></SkillBox>
-                <SkillBox
-                  skillTitle='Java'
-                  jobs={['UMD', 'Alertus', 'ITG']}
-                ></SkillBox>
-                <SkillBox
-                  skillTitle='Java'
-                  jobs={['UMD', 'Alertus', 'ITG']}
-                ></SkillBox>
-              </Card.Group>
-            </Container>
-            <pre></pre>
-            <Container fluid >
-              <Header as='h3' dividing>Tools, Libraries, and Frameworks</Header>
-              <Card.Group>              
-                <SkillBox
-                  skillTitle='React JS'
-                  logo={reactLogo}
-                  jobs={['psilospore.me']}
-                ></SkillBox>
-                <SkillBox
-                  skillTitle='Spring Framework'
-                  jobs={['ITG']}
-                ></SkillBox>
-                </Card.Group>
-            </Container>
-          </Container>
-          <Segment inverted vertical className='footer'>
-            <Header.Subheader as='h4' >
-              Built with React JS + semantic ui
-            </Header.Subheader>
-          </Segment>
+class LanguagesPage extends Component {
+  //TODO merge with ToolsLibrariesPage once skills externalized
+  render() {
+    return (
+      <Container fluid >
+        <Header as='h1' dividing>Languages</Header>
+        <Card.Group>
+          <SkillBox
+            id={0}          
+            skillTitle='Javascript'
+            jobs={['Alertus', 'Jibe', 'ITG', 'psilospore.me']}
+            description='ssdgdgs adsgdgasf afsafs asf  as fasfasfafs asfasfassaas afas asfa ssfas as asfasfasasf  asfasfasafs asffassaf asffasf'
+          ></SkillBox>
+          <SkillBox
+            id={1}
+            skillTitle='Java'
+            jobs={['UMD', 'Alertus', 'ITG']}
+          ></SkillBox>
+          <SkillBox
+            id={2}          
+            skillTitle='Java'
+            jobs={['UMD', 'Alertus', 'ITG']}
+          ></SkillBox>
+          <SkillBox
+            id={3}                    
+            skillTitle='Java'
+            jobs={['UMD', 'Alertus', 'ITG']}
+          ></SkillBox>
+          <SkillBox
+            id={4}          
+            skillTitle='Java'
+            jobs={['UMD', 'Alertus', 'ITG']}
+          ></SkillBox>
+        </Card.Group>
+      </Container>
+    )
+  }
+}
+
+class ToolsLibrariesPage extends Component {
+  render() {
+    return (
+      <Container fluid >
+        <Header as='h1' dividing>Tools, Libraries, and Frameworks</Header>
+        <Card.Group>              
+          <SkillBox
+            skillTitle='React JS'
+            logo={reactLogo}
+            jobs={['psilospore.me']}
+          ></SkillBox>
+          <SkillBox
+            skillTitle='Spring Framework'
+            jobs={['ITG']}
+          ></SkillBox>
+          </Card.Group>
+      </Container>
+    )
+  }
+}
+
+class App extends Component {
+
+  NUMBER_OF_PAGES = 2;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageNum: 0,
+    };
+  };
+
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.handleKeyDown.bind(this))
+  }
+
+  handleKeyDown = (event) => {
+    debugger;
+    if (event.key === 'ArrowLeft') {
+      if (this.state.pageNum > 0) {
+        this.setState({pageNum: this.state.pageNum - 1});
+      } else {
+        //TODO wiggle animation
+      }
+    } else if (event.key === 'ArrowRight') {
+      if (this.state.pageNum < this.NUMBER_OF_PAGES) {
+        this.setState({pageNum: this.state.pageNum + 1});
+      } else {
+        //TODO wiggle animation
+      }
+    }
+  }
+
+  
+
+  render() {
+
+    let page;
+    switch (this.state.pageNum) {
+      case 0:
+        page = <IntroPage></IntroPage>;
+        break;
+      case 1:
+        page = <LanguagesPage></LanguagesPage>
+        break;
+      case 2:
+        page = <ToolsLibrariesPage></ToolsLibrariesPage>
+        break;
+    }
+
+    return (
+      <div className="App" onKeyDown={this.handleKeyPress}>
+        {page}
+        <Footer></Footer>
       </div>
     );
   }

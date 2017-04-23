b419a945ddc918bf6c5c11d242f5dcd2066046f6
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-04-23 17:42:11 -0400

    Seperating Skills page from App.js

--- src/App.js
+++ src/App.js
@@ -7,36 +7,7 @@ import './css/App.css';
 import './css/SkillBox.css';
 
 import { Container, GridColumn, Grid, GridRow, Segment, Item, ItemHeader, Image, Header, Icon, Card, Dimmer, DimmerDimmable } from 'semantic-ui-react';
-
- const JOBS = [
-      {
-        title: 'React'
-      },
-      {
-        title: 'Javascript'
-      },
-      {
-        title: 'Java'
-      },
-      {
-        title: 'Ruby'
-      },
-      {
-        title: 'OCaml'
-      },
-      {
-        title: 'Hibernate'
-      },
-      {
-        title: 'Typescript'
-      },
-      {
-        title: 'Typescript'
-      },
-      {
-        title: 'Typescript'
-      },
-    ];
+import Skill from './components/Skill.js';
 
 
 class IntroPage extends Component {
@@ -79,55 +50,7 @@ const Footer = (props) => {
       </Segment>
     )
 }
-class SkillBox extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    let skillBoxClasses = 'box skillbox';
-    if (this.props.active) {
-      skillBoxClasses += ' selected-skillbox';
-    }
-    return (
-      <div className={skillBoxClasses} key={this.props.key} onClick={this.props.onClick}>
-        <div className='circle'></div>
-        <a className='skill-title'>{this.props.title}</a>
-      </div>
-    )
-  }
 
-}
-
-class SkillInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      slideLeft: true
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    if (prevProps !== this.props) { // Slide left if new props
-      this.setState({slideLeft: true});
-    }
-  }
-
-  render() {
-    let classes = 'box skill-info';
-    if (this.state.slideLeft) {
-      classes += ' slide-left';
-      // Remove class when complete so animation is triggered next time props are changed
-      setTimeout(() => this.setState({slideLeft: false}), 200);
-    }
-    return (
-      <div className={classes}>
-        <a className='skill-title'>{this.props.title}</a>
-        <h3>Where {this.props.location}</h3>
-        <p>{this.props.description}</p>
-      </div>
-    )
-  }
-}
 
 
 // Rename
@@ -152,18 +75,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pageNum: 1,
-      active: 0
+      pageNum: 1
     };
   };
-
-  skillBoxClicked(index) {
-    return (evt) => {
-      this.setState({active: index});
-      evt.stopPropagation();
-    };
-  }
-  
 
   handleKeyDownTouchMove = (event) => {
     console.log(event);
@@ -186,37 +100,13 @@ class App extends Component {
   
   render() {
 
-    let skills = JOBS.map((job, index) => {
-      return (
-        <SkillBox
-          onClick={this.skillBoxClicked(index)}
-          active={index === this.state.active}
-          {...job}
-        />
-      )
-    });
-
-    let selectedSkill = JOBS[this.state.active];
-    let selectedSkillInfo = selectedSkill ?
-        <SkillInfo
-          {...selectedSkill}
-          location='ITG'
-          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-          />: <div></div>;
-
-    let page;
+    let currentPage;
     switch (this.state.pageNum) {
       case 0:
-        page = <IntroPage></IntroPage>;
+        currentPage = <IntroPage></IntroPage>;
         break;
       case 1:
-        page = <div>
-          <h1 className='page-title'>Skills</h1>
-          <div className='container'>
-            {selectedSkillInfo}
-            {skills}
-          </div>
-        </div>
+        currentPage = <Skill></Skill>
         break;
     }
 
@@ -226,7 +116,7 @@ class App extends Component {
           pageNum={this.state.pageNum}
           NUMBER_OF_PAGES={this.NUMBER_OF_PAGES}
         ></Navigation>
-        {page}
+        {currentPage}
         <Footer
           showHelper={this.state.firstTime}
         ></Footer>

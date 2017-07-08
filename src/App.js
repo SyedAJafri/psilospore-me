496f51772d371a5b800ecb24a752683589eca492
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-07-07 21:08:14 -0400

    Using fullpage-react instead

--- src/App.js
+++ src/App.js
@@ -8,99 +8,119 @@ import { LanguagesPage, ToolsPage } from './components/Skill.js';
 import { JobsPage, EducationPage } from './components/Experience.js';
 import IntroPage from './components/Intro.js';
 import FinalPage from './components/Final.js';
-import Footer from './components/Footer.js';
+import PsilosporeFooter from './components/Footer.js';
 import Navigation from './components/Navigation.js';
+import {SectionsContainer, Section, Header, Footer} from 'react-fullpage';
+
+// class Example extends React.Component {
+
+//   PAGE_MAX_INDEX = 5;
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       current: 0,
+//       firstTime: true
+//     };
+//   };
+
+//   // componentWillMount() {
+//   //   document.addEventListener('keydown', this.handleKeyDownTouchMove.bind(this));
+//   //   document.addEventListener('touchmove', this.handleKeyDownTouchMove.bind(this));
+//   // }
+
+//   // componentWillUnmount(){
+//   //   document.removeEventListener('keydown', this.handleKeyDownTouchMove.bind(this))
+//   //   document.removeEventListener('touchmove', this.handleKeyDownTouchMove.bind(this));    
+//   // }
+
+//   // handleKeyDownTouchMove = (event) => {
+//   //   console.log(event);
+//   //   if (event.key === 'ArrowLeft') {
+//   //     if (this.state.pageNum > 0) {
+//   //       this.setState({pageNum: this.state.pageNum - 1});
+//   //       this.setState({firstTime: false});
+//   //     } else {
+//   //       //TODO wiggle animation
+//   //     }
+//   //   } else if (event.key === 'ArrowRight') {
+//   //     if (this.state.pageNum < this.PAGE_MAX_INDEX) {
+//   //       this.setState({pageNum: this.state.pageNum + 1});
+//   //       this.setState({firstTime: false});        
+//   //     } else {
+//   //       //TODO wiggle animation
+//   //     }
+//   //   } else if (!isNaN(event.key)) {
+//   //     const numEntered = Number.parseInt(event.key);
+//   //     if (1 <= numEntered && numEntered <= this.PAGE_MAX_INDEX + 1) {
+//   //       this.setState({pageNum: numEntered - 1});
+//   //       this.setState({firstTime: false});        
+//   //     }
+//   //   }
+//   // }
+  
+//   render() {
+
+//   let options = {
+//       scrollCallback: (states) => this.setState({current: states.activeSection}),
+//       arrowNavigation: true,
+//       scrollBar: false,
+//       navigation: true,
+//   };
+
+//     return (
+//       <SectionsContainer className="container" {...options} activeSection={this.state.current}>
+//           <Section className="custom-section" verticalAlign="true" color="#69D2E7">Page 1</Section>
+//           <Section color="#A7DBD8">Page 2</Section>
+//           <Section color="#E0E4CC">Page 3</Section>
+//       </SectionsContainer>
+
+//     );
+
+//   }
+// }
+
 
 class App extends React.Component {
-
-  PAGE_MAX_INDEX = 5;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      pageNum: 0,
-      firstTime: true
-    };
-  };
-
-  componentWillMount() {
-    document.addEventListener('keydown', this.handleKeyDownTouchMove.bind(this));
-    document.addEventListener('touchmove', this.handleKeyDownTouchMove.bind(this));
-  }
-
-  componentWillUnmount(){
-    document.removeEventListener('keydown', this.handleKeyDownTouchMove.bind(this))
-    document.removeEventListener('touchmove', this.handleKeyDownTouchMove.bind(this));    
-  }
-
-  handleKeyDownTouchMove = (event) => {
-    console.log(event);
-    if (event.key === 'ArrowLeft') {
-      if (this.state.pageNum > 0) {
-        this.setState({pageNum: this.state.pageNum - 1});
-        this.setState({firstTime: false});
-      } else {
-        //TODO wiggle animation
-      }
-    } else if (event.key === 'ArrowRight') {
-      if (this.state.pageNum < this.PAGE_MAX_INDEX) {
-        this.setState({pageNum: this.state.pageNum + 1});
-        this.setState({firstTime: false});        
-      } else {
-        //TODO wiggle animation
-      }
-    } else if (!isNaN(event.key)) {
-      const numEntered = Number.parseInt(event.key);
-      if (1 <= numEntered && numEntered <= this.PAGE_MAX_INDEX + 1) {
-        this.setState({pageNum: numEntered - 1});
-        this.setState({firstTime: false});        
+    constructor(props) {
+      super(props)
+      this.state = {
+        current: 0,
       }
     }
-  }
-  
-  render() {
 
-    let currentPage;
-    switch (this.state.pageNum) {
-      case 0:
-        currentPage = <IntroPage></IntroPage>;
-        break;
-      case 1:
-        currentPage = <EducationPage></EducationPage>
-        break;
-      case 2:
-        currentPage = <JobsPage></JobsPage>
-        break;
-      case 3:
-        currentPage = <LanguagesPage></LanguagesPage>
-        break;
-      case 4:
-        currentPage = <ToolsPage></ToolsPage>
-        break;
-      case 5:
-        currentPage = <FinalPage></FinalPage>
-        break;
-      default:
-        throw new Error('Invalid page number');
+    render() {
+        const options = {
+            sectionClassName: 'section',
+            anchors: ['Intro', 'Education', 'Jobs', 'Languages', 'Tools'],
+            scrollBar: false,
+            navigation: true,
+            verticalAlign: false,
+            sectionPaddingTop: '50px',
+            sectionPaddingBottom: '50px',
+            arrowNavigation: true,
+            scrollCallback: (states) => this.setState({current: states.activeSection})
+        };
+
+        const {current} = this.state
+
+        return (
+            <div>
+                <SectionsContainer className="container" {...options} activeSection={current}>
+                  <Section><IntroPage></IntroPage></Section>
+                  <Section><JobsPage></JobsPage></Section>
+                  <Section><EducationPage></EducationPage></Section>                  
+                  <Section><LanguagesPage></LanguagesPage></Section>
+                  <Section><ToolsPage></ToolsPage></Section>
+                </SectionsContainer>
+
+                <div className="btnGroup">
+                    <button onClick={() => this.setState({current: current - 1})} disabled={current === 0}>pre</button>
+                    <button onClick={() => this.setState({current: current + 1})} disabled={current === 5}>next</button>
+                </div>
+            </div>
+        )
     }
-
-    return (
-      <div className="App">
-        <Navigation
-          pageNum={this.state.pageNum}
-          PAGE_MAX_INDEX={this.PAGE_MAX_INDEX}
-        ></Navigation>
-        {currentPage}
-        <Footer
-          showHelper={this.state.firstTime}
-          finalPage={this.state.pageNum === this.PAGE_MAX_INDEX}
-        ></Footer>
-      </div>
-    );
-
-  }
 }
-
-
 
 export default App;
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -34,7 +34,7 @@ class SkillInfo extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState){
-    if (prevProps !== this.props) { // Slide left if new props
+    if (prevProps.title !== this.props.title) { // Slide left if new props
       this.setState({slideLeft: true});
     }
   }
@@ -91,7 +91,7 @@ class Skills extends React.Component {
     let selectedSkill = SKILLS_DATA[this.props.skillsType][this.state.active];
     let selectedSkillInfo = selectedSkill ?
         <SkillInfo
-          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          description=''
           {...selectedSkill}
           />: <div></div>;
     return (
